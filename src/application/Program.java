8b72b98929de6d841ca3899ee56be2cddd5016a2
Mateus Lopes <mateus_tiwu@hotmail.com>
2024-04-08 19:38:52 -0500

    Special move promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,8 +34,14 @@ public class Program {
                     ChessPosition target = UI.readChessPosition(sc);
 
                     ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                    if (capturedPiece != null)
+                    if (capturedPiece != null) {
                         captured.add(capturedPiece);
+                    }
+                    if (chessMatch.getPromoted() != null) {
+                        System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                        String type = sc.nextLine();
+                        chessMatch.replacePromotedPiece(type);
+                    }
                 } catch (ChessException e) {
                     System.out.println(e.getMessage());
                     sc.nextLine();
